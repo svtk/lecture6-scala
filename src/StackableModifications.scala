403aaa1635eee4dd5620ca5c867cec6a85ce65b8
svtk <svetlana.isakova@jetbrains.com>
2012-10-18 21:50:46 +0400

    corrected stackable modifications example

--- src/StackableModifications.scala
+++ src/StackableModifications.scala
@@ -18,16 +18,22 @@ trait Doubling extends IntQueue {
 class MyQueue extends BasicIntQueue with Doubling
 
 trait Incrementing extends IntQueue {
-  override def put(x: Int) { super.put(x + 1) }
+  abstract override def put(x: Int) { super.put(x + 1) }
 }
+
 trait Filtering extends IntQueue {
   abstract override def put(x: Int) {
     if (x >= 0) super.put(x)
   }
 }
 
-val queue = (new BasicIntQueue
-  with Incrementing with Filtering)
+object StackableModifications extends App {
+  val idq = (new BasicIntQueue with Incrementing with Doubling)
+  val diq = (new BasicIntQueue with Doubling with Incrementing)
 
-val queue1 = (new BasicIntQueue
-  with Filtering with Incrementing)
+  idq.put(2)
+  diq.put(2)
+
+  println(idq.get()) //5 = 2 * 2 + 1
+  println(diq.get()) //6 = (2 + 1) * 2
+}
